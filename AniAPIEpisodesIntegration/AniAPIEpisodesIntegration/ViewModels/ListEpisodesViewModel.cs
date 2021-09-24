bec024cbee1ca05ad420924a7915d9b72f9d5e8a
aledeltoro <alejandroencarnacion1@gmail.com>
2021-09-24 02:22:30 -0400

    Define view model to list episodes

---
+++ AniAPIEpisodesIntegration/AniAPIEpisodesIntegration/ViewModels/ListEpisodesViewModel.cs
@@ -0,0 +1,33 @@
+﻿using AniAPIEpisodesIntegration.Models;
+using AniAPIEpisodesIntegration.Services;
+using Refit;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AniAPIEpisodesIntegration.ViewModels
+{
+    public class ListEpisodesViewModel: BaseViewModel
+    {
+        public ObservableCollection<Models.Episode> EpisodesList { get; set; }
+
+        private IAniApiService _aniApiService;
+
+        public ListEpisodesViewModel() { }
+
+        public ListEpisodesViewModel(IAniApiService aniApiService)
+        {
+            _aniApiService = aniApiService;
+            LoadAnimeEpisodesAsync();
+        }
+
+        private async void LoadAnimeEpisodesAsync()
+        {
+            var response = await _aniApiService.GetEpisodesAsync();
+            EpisodesList = new ObservableCollection<Episode>(response.Data.Episodes);
+        }
+
+    }
+}
