c5899e570f7d7883c08d9f28c18a338b8275717d
aledeltoro <alejandroencarnacion1@gmail.com>
2021-09-24 02:22:52 -0400

    Implement ListEpisodes view

---
+++ AniAPIEpisodesIntegration/AniAPIEpisodesIntegration/Views/ListEpisodesView.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AniAPIEpisodesIntegration.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ListEpisodesView : ContentPage
+    {
+        public ListEpisodesView()
+        {
+            InitializeComponent();
+        }
+    }
+}
